690002407f176cc9cd85be11c1c060d64cef94bd
YotamBenda <59575705+YotamBenda@users.noreply.github.com>
2020-12-27 18:23:48 +0200

    lean touch implement

---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    [Header("Player Attributes")]
+    [SerializeField] private float speed = 20f;
+
+    void Update()
+    {
+        transform.position += Vector3.forward * speed * Time.deltaTime;
+    }
+}
