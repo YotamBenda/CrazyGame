d1e4210b3b83630f4f6e33820459f715ae50aaf0
YotamBenda <59575705+YotamBenda@users.noreply.github.com>
2020-12-31 02:10:46 +0200

    Cars moving!

---
+++ Assets/Scripts/Cars_Manager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cars_Manager : MonoBehaviour
+{
+    [Header("Instantiate")]
+    [SerializeField] private GameObject[] spawnPoints;
+    [SerializeField] private float spawnTimer = 5f;
+    [SerializeField] private GameObject[] cars;
+
+    private float count = 3f;
+
+    void Update()
+    {
+        count -= Time.deltaTime;
+        count = Mathf.Clamp(count, 0f, Mathf.Infinity);
+        if(count == 0)
+        {
+            StartCoroutine("SpawnCars");
+            count = spawnTimer;
+        }
+    }
+
+    private IEnumerator SpawnCars()
+    {
+        int carNum = Random.Range(0, cars.Length);
+        int pointNum = Random.Range(0, spawnPoints.Length);
+        var currCar = Instantiate(cars[carNum], spawnPoints[pointNum].transform.position, Quaternion.identity);
+        var offset = new Vector3(-90, 0, -90);
+        var offset2 = new Vector3(-90, 0, 90);
+        if(carNum == 0)
+            currCar.transform.Rotate(offset2);
+        else
+            currCar.transform.Rotate(offset);
+        yield return new WaitForSeconds(spawnTimer);
+    }
+
+
+}
---
+++ Assets/Scripts/Cars_Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cars_Movement : MonoBehaviour
+{
+    [Header("Car's Attributes")]
+    [SerializeField] private float speed = 5f; //debug
+    [SerializeField] private GameObject startPoint;
+
+    void Update()
+    {
+        transform.localPosition += Vector3.back * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hit" + other.gameObject.name);
+        if(other.gameObject.tag == "CarsRoadEnd")
+        {
+            Debug.Log("car reached end");
+            transform.position = startPoint.transform.position;
+        }
+    }
+}
